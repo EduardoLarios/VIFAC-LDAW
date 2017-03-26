eb55d286f8b9f8290542f9797b19027fc06804ee
Esteban <a01184855@itesm.mx>
2017-03-25 19:04:54 -0600

    Scholar module done

---
+++ VIFAC/scholar/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Escuela, Material
+
+
+class AsignarMaterial(forms.ModelForm):
+    
+    class Meta:
+        model = Material
+        fields = ['descripcion', 'categoria']
+    
+        
+class EscuelaForm(forms.ModelForm):
+    
+    class Meta:
+        model = Escuela
+        fields = ['name', 'ciudad', 'estado']
--- VIFAC/scholar/models.py
+++ VIFAC/scholar/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Material(models.Model):
+    categoria = models.CharField(
+        max_length=128,
+        verbose_name='Category Name',
+        help_text="Nombre de categoria"
+    )
+    descripcion = models.CharField(
+        max_length=256,
+        verbose_name='Description',
+        help_text='Descripcion del producto'
+    )
+    entrego =  models.CharField(
+        max_length=256,
+        verbose_name='Quien entrego',
+        help_text='Persona que entrego el material'
+    )
+    escuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.descripcion
+    
+
+class Escuela(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Nombre de la Escuela',
+        help_text='Nombre de la Escuela'
+    )
+    ciudad = models.CharField(
+        max_length=256,
+        verbose_name='Ciudad',
+        help_text='Ciudad de la escuela'
+    )
+    estado = models.CharField(
+        max_length=256,
+        verbose_name='Estado',
+        help_text='Estado donde está la escuela'
+    )
+    
+    def __str__(self):
+        return self.name
