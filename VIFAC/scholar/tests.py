cbb8e86561665a91669bcc1de1c1287e5bfec49d
Esteban <a01184855@itesm.mx>
2017-03-31 01:41:35 -0600

    16 tests implementados de usuarios y escuelas

--- VIFAC/scholar/tests.py
+++ VIFAC/scholar/tests.py
@@ -1,3 +1,68 @@
 from django.test import TestCase
+from .models import Escuela, Material
+from django.db.models import QuerySet
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        Escuela.objects.create(name='test1', ciudad='test1', estado='test1')
+        Escuela.objects.create(name='test2', ciudad='test2', estado='test2')
+    
+    def test_create_school(self):
+        Escuela.objects.create(name='create', ciudad='test', estado='test')
+        school_count = Escuela.objects.all().count()
+        
+        self.assertEqual(school_count, 3)
+        
+    def test_school_exists(self):
+        school_counts = Escuela.objects.filter(name='test1').count()
+        
+        self.assertEqual(school_counts, 1)
+
+    def test_delete_school(self):
+        school = Escuela.objects.get(name='test2')
+        school.delete()
+        school_count = Escuela.objects.all().count()
+        
+        self.assertEqual(school_count, 1)
+        
+    def test_list_user(self):
+        queryset =  Escuela.objects.all()
+        
+        self.assertIsInstance(queryset, QuerySet)
+        
+    def test_assign_material(self):
+        school = Escuela.objects.get(name='test1')
+        Material.objects.create(categoria='ejemplo', descripcion='ejemplo', entrego='ejemplo', escuela=school)
+        material_count = Material.objects.all().count()
+        
+        self.assertEqual(material_count, 1)
+        
+    def test_list_material(self):
+        school = Escuela.objects.get(name='test1')
+        Material.objects.create(categoria='ejemplo', descripcion='ejemplo', entrego='ejemplo', escuela=school)
+        queryset = Material.objects.all()
+        
+        self.assertIsInstance(queryset, QuerySet)
+        
+    def test_remove_material(self):
+        school = Escuela.objects.get(name='test1')
+        Material.objects.create(categoria='ejemplo', descripcion='ejemplo', entrego='ejemplo', escuela=school)
+        material = Material.objects.get(escuela=school)
+        material.delete()
+        material_count = Material.objects.all().count()
+        
+        self.assertEqual(material_count, 0)
+    
+    def test_edit_school(self):
+        school = Escuela.objects.get(name='test1')
+        school.name = 'nuevo nombre'
+        
+        self.assertEqual(school.name, 'nuevo nombre')
+        
+    def test_edit_material(self):
+        school = Escuela.objects.get(name='test1')
+        Material.objects.create(categoria='ejemplo', descripcion='ejemplo', entrego='ejemplo', escuela=school)
+        material = Material.objects.get(descripcion='ejemplo')
+        material.descripcion = 'nueva descripcion'
+        self.assertEqual(material.descripcion, 'nueva descripcion')
--- VIFAC/users/tests.py
+++ VIFAC/users/tests.py
@@ -1,14 +1,20 @@
+from django.contrib.auth import authenticate
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.db.models import QuerySet
 from rolepermissions.roles import assign_role, remove_role
+from django.test import Client
 
 
 class UserTestCase(TestCase):
     def setUp(self):
         User.objects.create(username='test1', password='test', first_name = 'test', last_name = 'test')
         User.objects.create(username='test2', password='test2', first_name='test2', last_name='test2')
-        
+        self.credentials = {
+            'username': 'esteban',
+            'password': 'gordolobo1'}
+        User.objects.create_user(**self.credentials)
+            
     def test_user_exists(self):
         user_counts = User.objects.filter(username='test1').count()
         self.assertEqual(user_counts, 1)
@@ -17,7 +23,7 @@ class UserTestCase(TestCase):
        user = User.objects.get(username='test2')
        user.delete()
        user_count = User.objects.all().count()
-       self.assertEqual(user_count,1)
+       self.assertEqual(user_count,2)
        
     def test_list_user(self):
         queryset =  User.objects.all()
@@ -30,3 +36,24 @@ class UserTestCase(TestCase):
     def test_remove_role(self):
         user = User.objects.get(username='test1')
         remove_role(user,'admin')
+        
+    def test_login(self):
+        self.c = Client()
+        self.user = User.objects.create(username='testuser', password='12345', is_active=True, is_staff=True,
+                                        is_superuser=True)
+        self.user.set_password('hello')
+        self.user.save()
+        self.user = authenticate(username='testuser', password='hello')
+        login = self.c.login(username='testuser', password='hello')
+        self.assertTrue(login)
+        
+    def test_logout(self):
+        self.c = Client()
+        self.user = User.objects.create(username='testuser', password='12345', is_active=True, is_staff=True,
+                                        is_superuser=True)
+        self.user.set_password('hello')
+        self.user.save()
+        self.user = authenticate(username='testuser', password='hello')
+        login = self.c.login(username='testuser', password='hello')
+        logout = self.c.logout()
+        self.assertTrue(logout == None)
